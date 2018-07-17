4c6664e5f77e514fad1036209c3b194cc13fe5bd
peggy <peggy@maicoin.com>
2018-07-17 11:30:38 +0800

    build: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './lib/index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'max-exchange-api.js',
