e45a85bdcfd449c90d2f8a853c75f07443f7b42e
c9s <yoan@maicoin.com>
2021-02-26 13:44:26 +0800

    add public trades

---
+++ examples/ws-trade.js
@@ -0,0 +1,12 @@
+
+const WebSocketAPI = require('../lib/transports/websocket')
+
+const ws = new WebSocketAPI({ accessKey: '', secretKey: '' })
+
+ws.subscribe('trade', 'btctwd')
+
+ws.on('trade.snapshot', (e) => { console.log(e) })
+ws.on('trade.update', (e) => { console.log(e) })
+
+ws.on('raw', (body) => console.log(body))
+ws.connect()
